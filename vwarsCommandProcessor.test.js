5e032288645a7c7519c70d592157eb4be579ca20
general-ronimus <general.ronimus@gmail.com>
2023-06-20 21:29:35 -0500

    Sample payloads for vwa and stealth command tests

--- vwarsCommandProcessor.test.js
+++ vwarsCommandProcessor.test.js
@@ -150,4 +150,13 @@ test('nuke', async () => {
   expect(response.statusCode).toBe(200);
 });
 
+/*
+test('stealth strike', async () => {
+  let slashCommandBody = fs.readFileSync("testResources/sampleSlashCommandBody_stealth_strike.json")
+  let slashCommandBodyJson = JSON.parse(slashCommandBody)
+  let response = await vwarsCommandProcessor.process(slashCommandBodyJson)
+  console.log(response)
+  expect(response.statusCode).toBe(200);
+});
+*/
 
